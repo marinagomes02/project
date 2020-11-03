2e49bc6aaa14fc79a4de0eeff6169ae5b3162bb6
marinagomes02 <marina.gomes02@gmail.com>
2020-11-03 23:05:05 +0000

    projeto po

---
+++ woo-core/src/woo/Store.java
@@ -0,0 +1,28 @@
+package woo;
+
+import java.io.*;
+import woo.exceptions.*;
+
+/**
+ * Class Store implements a store.
+ */
+public class Store implements Serializable {
+
+  /** Serial number for serialization. */
+  private static final long serialVersionUID = 202009192006L;
+
+  // FIXME define attributes
+  // FIXME define contructor(s)
+  // FIXME define methods
+
+  /**
+   * @param txtfile filename to be loaded.
+   * @throws IOException
+   * @throws BadEntryException
+   */
+  void importFile(String txtfile) throws IOException, BadEntryException, ImportFileException {
+    
+    BufferedReader in
+  }
+
+}
